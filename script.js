352b9363e06e05b6cdd43961763ac14ca313fb7f
dci-student <oliver.sporl@dci-student.org>
2023-07-17 09:53:51 +0200

    readme gelöscht

--- script.js
+++ script.js
@@ -5,16 +5,19 @@ function add(){
     var num2 = Number(document.getElementById('num2').value);
     document.getElementById('result').innerHTML = num1 + num2;
 }
+
 function subtract(){
     var num1 = Number(document.getElementById('num1').value);
     var num2 = Number(document.getElementById('num2').value);
     document.getElementById('result').innerHTML = num1 - num2;
 }
+
 function multiply(){
     var num1 = Number(document.getElementById('num1').value);
     var num2 = Number(document.getElementById('num2').value);
     document.getElementById('result').innerHTML = num1 * num2;
 }
+
 function divide(){
     var num1 = Number(document.getElementById('num1').value);
     var num2 = Number(document.getElementById('num2').value);
